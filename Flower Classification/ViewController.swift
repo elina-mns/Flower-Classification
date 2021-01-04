66cc004e739689750124802faffc1c1a5f8219f1
elina-mns <ellie.mansurova@gmail.com>
2021-01-04 12:34:09 -0500

    import CoreML, Vision and implement detect method

--- Flower Classification/ViewController.swift
+++ Flower Classification/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
@@ -16,18 +18,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let pickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        imageView.image = pickedImage as? UIImage
+        if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("Couldn't convert to CIImage")
+            }
+            imageView.image = pickedImage
+            detect(flowerImage: ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func detect(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else { fatalError("Loading CoreML model failed") }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("Could not convert results") }
+            if let firstResult = results.first {
+                if let name = firstResult.identifier.contains(FlowerClassifier().model.description) {
+                    self.navigationItem.title = name
+                } else {
+                    self.navigationItem.title = "Couldn't find the flower type for this picture"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
 
