836fea0ef36d8baf57e3cd7dee66328a5d8ae693
elina-mns <ellie.mansurova@gmail.com>
2021-01-04 14:58:02 -0500

    fill text label with the info on flower type from wiki

--- Flower Classification/ViewController.swift
+++ Flower Classification/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
 
     @IBOutlet weak var imageView: UIImageView!
     
+    let baseURL = "https://en.wikipedia.org/w/api.php"
+    
+    @IBOutlet weak var textLabelfromWiki: UILabel!
+    
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -45,12 +49,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         let request = VNCoreMLRequest(model: model) { (request, error) in
             guard let results = request.results?.first as? VNClassificationObservation else { fatalError("Could not convert results") }
             self.navigationItem.title = results.identifier.capitalized
+            self.requestInfo(flowerName: results.identifier)
         }
         let handler = VNImageRequestHandler(ciImage: flowerImage)
         do {
             try handler.perform([request])
         } catch {
             print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String) {
+        
+        let requiredParameters: [String: String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+            
+        ]
+            
+        Alamofire.request(baseURL, method: .get, parameters: requiredParameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                let flowerJSON: JSON = JSON(response.result.value!)
+                let pageId = flowerJSON["query"]["pageids"][0].stringValue
+                let flowerDescription = flowerJSON["query"]["pages"][pageId]["extract"].stringValue
+                self.textLabelfromWiki.text = flowerDescription
+            }
         }
     }
 }
